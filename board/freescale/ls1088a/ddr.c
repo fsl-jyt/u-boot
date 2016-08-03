4e57f27313acb407b7f011d3fa9261b7b136a8e0
Shengzhou Liu <Shengzhou.Liu@nxp.com>
2016-08-03 15:35:20 +0800

    armv8: ls1088a: Update DDR config for ls1088aqds and ls1088ardb
    - Update clk_adj to match latest ddr driver.
    - Enable ddr data bus width to 64bit default.
    - Remove unused ddr raw timing.
    
    Signed-off-by: Shengzhou Liu <Shengzhou.Liu@nxp.com>

--- board/freescale/ls1088a/ddr.c
+++ board/freescale/ls1088a/ddr.c
@@ -63,16 +63,13 @@ void fsl_ddr_board_options(memctl_options_t *popts,
 		panic("DIMM is not supported by this board");
 	}
 found:
-#if defined(CONFIG_TARGET_LS1088AQDS) || defined(CONFIG_EMU)
 	debug("Found timing match: n_ranks %d, data rate %d, rank_gb %d\n"
 		"\tclk_adjust %d, wrlvl_start %d, wrlvl_ctrl_2 0x%x, wrlvl_ctrl_3 0x%x\n",
 		pbsp->n_ranks, pbsp->datarate_mhz_high, pbsp->rank_gb,
 		pbsp->clk_adjust, pbsp->wrlvl_start, pbsp->wrlvl_ctl_2,
 		pbsp->wrlvl_ctl_3);
-#else
-	debug("Found timing match: n_ranks %d, data rate %d, rank_gb %d\n",
-	      pbsp->n_ranks, pbsp->datarate_mhz_high, pbsp->rank_gb);
 
+#ifdef CONFIG_FORCE_DDR_DATA_BUS_WIDTH_32
 	pdimm[0].dq_mapping[0] = 0x15;
 	pdimm[0].dq_mapping[1] = 0x35;
 	pdimm[0].dq_mapping[2] = 0x0b;
@@ -91,23 +88,22 @@ found:
 	pdimm[0].dq_mapping[15] = 0;
 	pdimm[0].dq_mapping[16] = 0;
 	pdimm[0].dq_mapping[17] = 0;
+	popts->data_bus_width = DDR_DATA_BUS_WIDTH_32;
+#else
+	/* default usually we set DDR data bus width to 64-bit */
+	popts->data_bus_width = DDR_DATA_BUS_WIDTH_64;
+#endif
 
-	/* force DDR bus width to 32 bits */
-	popts->data_bus_width = 1;
 	popts->otf_burst_chop_en = 0;
 	popts->burst_length = DDR_BL8;
 	popts->bstopre = 0;	     /* enable auto precharge */
-#endif
 
 	/*
 	 * Factors to consider for half-strength driver enable:
 	 *	- number of DIMMs installed
 	 */
-#if defined(CONFIG_TARGET_LS1088AQDS) || defined(CONFIG_EMU)
-	popts->half_strength_driver_enable = 1;
-#else
 	popts->half_strength_driver_enable = 0;
-#endif
+
 	/*
 	 * Write leveling override
 	 */
@@ -133,73 +129,6 @@ found:
 #endif
 }
 
-#ifdef CONFIG_SYS_DDR_RAW_TIMING
-dimm_params_t ddr_raw_timing = {
-	.n_ranks = 1,
-	.rank_density = 4294967296u,
-	.capacity = 4294967296u,
-	.primary_sdram_width = 64,
-	.ec_sdram_width = 8,
-	.registered_dimm = 0,
-	.mirrored_dimm = 1,
-	.n_row_addr = 15,
-	.n_col_addr = 10,
-	.bank_addr_bits = 0,
-	.bank_group_bits = 2,
-	.edc_config = 2,
-	.burst_lengths_bitmask = 0x0c,
-
-	.tckmin_x_ps = 938,
-	.tckmax_ps = 1500,
-	.caslat_x = 0x000DFA00,
-	.taa_ps = 13500,
-	.trcd_ps = 13500,
-	.trp_ps = 13500,
-	.tras_ps = 33000,
-	.trc_ps = 46500,
-	.trfc1_ps = 260000,
-	.trfc2_ps = 160000,
-	.trfc4_ps = 110000,
-	.tfaw_ps = 21000,
-	.trrds_ps = 3700,
-	.trrdl_ps = 5300,
-	.tccdl_ps = 5355,
-	.refresh_rate_ps = 7800000,
-	.dq_mapping[0] = 0x00,
-	.dq_mapping[1] = 0x00,
-	.dq_mapping[2] = 0x00,
-	.dq_mapping[3] = 0x00,
-	.dq_mapping[4] = 0x00,
-	.dq_mapping[5] = 0x00,
-	.dq_mapping[6] = 0x00,
-	.dq_mapping[7] = 0x00,
-	.dq_mapping[8] = 0x00,
-	.dq_mapping[9] = 0x00,
-	.dq_mapping[10] = 0x00,
-	.dq_mapping[11] = 0x00,
-	.dq_mapping[12] = 0x00,
-	.dq_mapping[13] = 0x00,
-	.dq_mapping[14] = 0x00,
-	.dq_mapping[15] = 0x00,
-	.dq_mapping[16] = 0x00,
-	.dq_mapping[17] = 0x00,
-	.dq_mapping_ors = 1,
-};
-
-int fsl_ddr_get_dimm_params(dimm_params_t *pdimm,
-			    unsigned int controller_number,
-			    unsigned int dimm_number)
-{
-	static const char dimm_model[] = "Fixed DDR on board";
-
-	if (dimm_number == 0)
-		memcpy(pdimm, &ddr_raw_timing, sizeof(dimm_params_t));
-		memset(pdimm->mpart, 0, sizeof(pdimm->mpart));
-		memcpy(pdimm->mpart, dimm_model, sizeof(dimm_model) - 1);
-
-	return 0;
-}
-#endif
 
 phys_size_t initdram(int board_type)
 {
