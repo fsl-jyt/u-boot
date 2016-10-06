9813591b57172c865899dea5e195f8067392a1a6
Raghav Dogra <raghav.dogra@nxp.com>
2016-10-06 22:53:56 +0530

    ls1088a: mmc: revert the sd_change_freq avoidance
    sd_change_freq had been disabled due to an error. This patch removes it
    as it is no longer required. MMC DMA is now able to access the OCRAM.
    
    Signed-off-by: Raghav Dogra <raghav.dogra@nxp.com>

--- drivers/mmc/mmc.c
+++ drivers/mmc/mmc.c
@@ -1338,7 +1338,6 @@ static int mmc_startup(struct mmc *mmc)
 	if (err)
 		return err;
 
-#if !(defined(CONFIG_LS1088A) && defined(CONFIG_SD_BOOT))
 	if (IS_SD(mmc))
 		err = sd_change_freq(mmc);
 	else
@@ -1346,7 +1345,6 @@ static int mmc_startup(struct mmc *mmc)
 
 	if (err)
 		return err;
-#endif
 
 	/* Restrict card's capabilities by what the host can do */
 	mmc->card_caps &= mmc->cfg->host_caps;
